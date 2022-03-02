e450d5dd810385d73d74593021d7f686cd5e3ad7
Hareesh Sikakollu <hareeshsikakollu@gmail.com>
2022-03-02 13:38:52 +0530

    Correcting theme

--- src/components/pages/ePage/EPage.tsx
+++ src/components/pages/ePage/EPage.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import { ThemeProvider } from '@emotion/react';
 import { SearchField } from '../../molecules/searchBar/SearchBar';
 import Banner from '../../molecules/banner/Banner';
 import { EBooks } from '../../organisms/eBooks/EBooks';
 import Footer from '../../organisms/footer/Footer';
 import Header from '../../organisms/header/Header';
+import theme from '../../../Theme/theme';
 
 function EPage() {
   return (
     <div>
-      <Header />
-      <Banner />
-      <SearchField />
-      <EBooks />
-      <Footer />
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Banner />
+        <SearchField />
+        <EBooks />
+        <Footer />
+      </ThemeProvider>
     </div>
   );
 }
